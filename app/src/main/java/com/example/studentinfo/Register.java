10f1e8d0a9fdbb29198e61468a019c475573cb12
Anandkonda765 <anand@vedaslabs.com>
2022-11-18 17:34:48 +0530

    Date is set

--- app/src/main/java/com/example/studentinfo/Register.java
+++ app/src/main/java/com/example/studentinfo/Register.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,8 +22,9 @@ import java.util.Calendar;
 public class Register extends AppCompatActivity {
     EditText email, password, username, Dob, age, phoneno;
     RadioGroup genderGroup;
-    RadioButton genderButton;
-
+    RadioButton genderMale,genderFemale;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     Button submit;
     boolean isAllFieldsChecked = false;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
@@ -42,11 +44,9 @@ public class Register extends AppCompatActivity {
         dobActions();
         age = findViewById(R.id.age);
         phoneno = findViewById(R.id.phonenumber);
-        genderGroup = findViewById(R.id.gender);
-
-        int selectedId = genderGroup.getCheckedRadioButtonId();
-        genderButton = findViewById(selectedId);
-
+        genderActions();
+       /* int selectedId = genderGroup.getCheckedRadioButtonId();
+        genderButton = findViewById(selectedId);*/
         submit = findViewById(R.id.submit);
 
 
@@ -59,12 +59,41 @@ public class Register extends AppCompatActivity {
             password.setText(info.getPassword());
             Dob.setText(info.getDob());
             phoneno.setText(info.getPhoneno());
+
            // genderButton.setChecked(true);
 
 
         }
 
         registerActions();
+
+
+    }
+    private void genderActions() {
+        genderGroup = findViewById(R.id.gender);
+        genderMale=findViewById(R.id.Male);
+        genderFemale=findViewById(R.id.Female);
+        sharedPreferences=getSharedPreferences("gender",0);
+        int gender= sharedPreferences.getInt("gender",3);
+        editor=sharedPreferences.edit();
+        if(gender==1){
+            genderMale.setChecked(true);
+        }else if(gender==0){
+            genderFemale.setChecked(true);
+        }
+        genderGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.Male) {
+                    editor.putInt("gender", 1);
+                } else if (checkedId == R.id.Female) {
+                    editor.putInt("gender", 0);
+                }
+                editor.commit();
+            }
+        });
+
+
 
 
     }
@@ -102,18 +131,19 @@ public class Register extends AppCompatActivity {
                     studentInfo.setUsername(username.getText().toString());
                     studentInfo.setPhoneno(phoneno.getText().toString());
                     studentInfo.setAge(age.getText().toString());
-
-                    int selectedId = genderGroup.getCheckedRadioButtonId();
-                    genderButton = findViewById(selectedId);
-
-                    studentInfo.setGender(genderButton.getText().toString());
-
-
+                    studentInfo.setDob(Dob.getText().toString());
                     studentInfo.setPassword(password.getText().toString());
+
+                    //int selectedId = genderGroup.getCheckedRadioButtonId();
+                    //genderButton = findViewById(selectedId);
+
+                   // studentInfo.setGender(genderButton.getText().toString());
+
+
                     if (StudentDataController.getInstance().currentUser != null) {
                         StudentDataController.getInstance().currentUser = studentInfo;
                         StudentDataController.getInstance().updateUserData(StudentDataController.getInstance().currentUser);
-                        Toast.makeText(getApplicationContext(), "update data successfullyu", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "update data successfully", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(Register.this, Home.class);
                         startActivity(intent);
                     } else {
@@ -151,7 +181,7 @@ public class Register extends AppCompatActivity {
         }
 
 
-        
+
         if (Dob.length() == 0) {
             Dob.setError("Date of birth is required");
             return false;
@@ -159,7 +189,8 @@ public class Register extends AppCompatActivity {
         if (age.length() == 0) {
             age.setError("country is required");
             return false;
-        } else if (phoneno.length() == 0) {
+        }
+        else if (phoneno.length() == 0) {
             phoneno.setError("phoneno is required");
             return false;
         }
